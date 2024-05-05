1621fe9dda5ea93220ae487629aac4e00786e9f9
dharmatech <wayo.cavazos@gmail.com>
2024-05-05 16:47:09 -0700

    load_records

--- newyorkfed_pandas_rrp.py
+++ newyorkfed_pandas_rrp.py
@@ -19,6 +19,10 @@ def download_records_after(date):
 
         return df
 
+# ----------------------------------------------------------------------
+# update_records is deprecated.
+# Use load_records instead.
+# ----------------------------------------------------------------------
 def update_records(start_date):
     
     path = 'rrp.pkl'
@@ -54,5 +58,42 @@ def update_records(start_date):
         df.to_pickle(path)
 
         return df
+# ----------------------------------------------------------------------
+def load_records(start_date, update=False):
+    
+    path = 'rrp.pkl'
 
-# result = update_records('2024-01-01')
+    if os.path.isfile(path):
+
+        print(f'Found {path}. Importing.')
+
+        df = pd.read_pickle(path)
+
+        if update == False:
+            return df
+
+        recent_date = df['operationDate'].iloc[1]
+
+        print(f'Second most recent date: {recent_date}.')
+
+        new_records = download_records_after(recent_date)
+
+        existing_records = df[df['operationDate'] < recent_date]
+
+        # new_df = pd.concat([existing_records, new_records], ignore_index=True)
+
+        new_df = pd.concat([new_records, existing_records], ignore_index=True)
+
+        new_df.to_pickle(path)
+
+        return new_df
+    
+    else:
+
+        print(f'No {path} found. Downloading all records after {start_date}.')
+
+        df = download_records_after(start_date)
+
+        df.to_pickle(path)
+
+        return df
